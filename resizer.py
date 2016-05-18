e4b66f4546e0a4612fd5149df57fa536066444dc
Adam Vilmin <crappyoats@gmail.com>
2016-05-17 21:35:30 -0500

    stuck with wand

--- resizer.py
+++ resizer.py
@@ -1,4 +1,6 @@
 import os, glob, time, sys
+from wand.image import Image
+from math import floor
 
 def resizer():
     """opens all files in CWD, resizes to 10% of original file, and renames to
@@ -18,11 +20,17 @@ def resizer():
         pictures.extend(glob.glob(files))
 
     i = 0    
-    for filename in pictures:
+    for pic in pictures:
+        with Image(filename = pic) as img:
+            wid = float(img.width)
+            hi = float(img.height)
+            print(str(wid) + " " + str(hi))
+            #img.resize(floor(wid * .1), floor(hi * .1))
+            img.save(filename = pic)
         
-        type = filename.split('.')[1]
+        type = pic.split('.')[1]
         new_name = time.strftime("%Y-%m-%d")+"-"+venue+"-"+str(i)+"."+type
-        os.rename(filename, new_name)
+        os.rename(pic, new_name)
         i += 1
         
 if __name__ == '__main__':
